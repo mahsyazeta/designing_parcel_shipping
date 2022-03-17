7297f0e6d12a154624f8735d464cd2414d6dfafd
mahsyazeta <zetadhana@gmail.com>
2022-03-17 07:31:14 +0700

    update part 2

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:designing_parcel_shipping/parcel_app_theme.dart';
+import 'package:designing_parcel_shipping/ui/screens/screens.dart';
+import 'package:designing_parcel_shipping/ui/widgets/widgets.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ParcelAppTheme.lightTheme,
+      home: const Scaffold(
+        body: SendParcelCheckoutScreen(),
+        bottomNavigationBar: MyBottomNavigationBar(),
+      ),
+    );
+  }
+}
